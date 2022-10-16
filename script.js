65b3d94741019e84cfca59563a002386c6cce531
mitsuya-creator <jj4068851@gmail.com>
2022-10-16 17:59:15 +0700

    evalFunction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let a=1,b = '2';
-//crewt using $ and eval() will return sting
+//creat using $ and eval() will return sting
 //console.log(typeof`${eval(a)}`);
 console.log("this line use $",typeof`${a}`)
 console.log(typeof(a))
