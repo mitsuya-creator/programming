8ba6b8fff443f000618f2fbbe9057b7b8079ae6a
mitsuya-creator <jj4068851@gmail.com>
2022-10-16 21:11:44 +0700

    isFinite

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ let a=1,b = '2';
 console.log("this line use $",typeof`${a}`)
 //return number
 console.log(typeof(a))
+
+//isfinite function, to test value is not Infinity
+function div(angkaUser){
+  if(isFinite(1000/angkaUser)){
+    return "number is not infinity"
+    
+  }
+  return "number is infinite"
+}
+
+console.log(div(0))
+console.log(div(1))
