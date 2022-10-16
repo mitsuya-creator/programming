408767caef118d5a906167697d5b58d9fdd9eaf8
mitsuya-creator <jj4068851@gmail.com>
2022-10-16 18:01:32 +0700

    evalFunction

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let a=1,b = '2';
 //creat using $ and eval() will return sting
-//console.log(typeof`${eval(a)}`);
+
+//returm string
 console.log("this line use $",typeof`${a}`)
+//return number
 console.log(typeof(a))
