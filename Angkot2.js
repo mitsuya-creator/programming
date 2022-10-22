e14a8a8ea5ad385b1d4a1f3dc3a4c6560b509b0e
mitsuya-creator <jj4068851@gmail.com>
2022-10-23 03:29:06 +0700

    switch

--- Angkot2.js
+++ Angkot2.js
@@ -1,20 +1,17 @@
-/* Angkot js 
-Angkot no 1-4 dan 6 baik
-Angkot no 7 dan 9 rusak
-Angkot no 5,8 dan 10 lembur
-*/
-var jmlAngkot = 10;
-var AngkotBeroperasi = 8;
-var noAngkot = 1;
+/* SWITCH CONDITION */
 
-for(noAngkot;noAngkot<=jmlAngkot;noAngkot++){
-  if(noAngkot<=4||noAngkot==6){
-    console.log(`Angkot no ${noAngkot} baik`)
-  }
-  else if(noAngkot== 5||noAngkot==8||noAngkot==10){
-    console.log(`Angkot no ${noAngkot} lembur`)
-  }
-  else{
-    console.log(`Angkot no ${noAngkot} rusak`)
-  }
+switch(3){
+  case 1:
+    console.log('angka 1')
+    break;
+  case 2:
+    console.log('angka 2')
+    break;
+  case 3:
+    console.log('angka 3')
+    break;
+  case 4:
+    console.log('angka 4')
+  default:
+    console.log('your not choose anything')
 }
