3e16ed9be296a192f1a843cc87ecb9bc53b24564
mitsuya-creator <jj4068851@gmail.com>
2022-10-22 18:25:47 +0700

    forFunction

---
+++ Angkot2.js
@@ -0,0 +1,11 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 9;
+var noAngkot = 1;
+
+for(noAngkot;noAngkot<=jmlAngkot;noAngkot++){
+  if(noAngkot<=angkotBeroperasi){
+    console.log(`angkot no ${noAngkot} baik`)
+  }else{
+    console.log(`angkot no ${noAngkot} rusak`)
+  }
+}
