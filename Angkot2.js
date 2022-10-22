a5686572cd69aa193a92daef590fd3e081745b8b
mitsuya-creator <jj4068851@gmail.com>
2022-10-23 03:04:23 +0700

    opLogic

--- Angkot2.js
+++ Angkot2.js
@@ -1,11 +1,20 @@
+/* Angkot js 
+Angkot no 1-4 dan 6 baik
+Angkot no 7 dan 9 rusak
+Angkot no 5,8 dan 10 lembur
+*/
 var jmlAngkot = 10;
-var angkotBeroperasi = 5;
+var AngkotBeroperasi = 8;
 var noAngkot = 1;
 
 for(noAngkot;noAngkot<=jmlAngkot;noAngkot++){
-  if(noAngkot<=angkotBeroperasi){
-    console.log(`Angkot no ${noAngkot} BAIK`)
-  }else{
-    console.log(`Angkot no ${noAngkot} RUSAK`)
+  if(noAngkot<=4||noAngkot==6){
+    console.log(`Angkot no ${noAngkot} baik`)
+  }
+  else if(noAngkot== 5||noAngkot==8||noAngkot==10){
+    console.log(`Angkot no ${noAngkot} lembur`)
+  }
+  else{
+    console.log(`Angkot no ${noAngkot} rusak`)
   }
 }
