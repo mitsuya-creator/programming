030b8e4faa2f70699818c100b9d423cb3ca57228
mitsuya-creator <jj4068851@gmail.com>
2022-10-22 20:28:06 +0700

    forDeleted

--- Angkot2.js
+++ Angkot2.js
@@ -2,10 +2,3 @@ var jmlAngkot = 10;
 var angkotBeroperasi = 9;
 var noAngkot = 1;
 
-for(noAngkot;noAngkot<=jmlAngkot;noAngkot++){
-  if(noAngkot<=angkotBeroperasi){
-    console.log(`angkot no ${noAngkot} baik`)
-  }else{
-    console.log(`angkot no ${noAngkot} rusak`)
-  }
-}
