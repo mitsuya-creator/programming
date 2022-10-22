62d5527d37de866fb7d7d83da99ea71d3bf63116
mitsuya-creator <jj4068851@gmail.com>
2022-10-22 23:18:02 +0700

    addForIf

--- Angkot2.js
+++ Angkot2.js
@@ -1,4 +1,11 @@
 var jmlAngkot = 10;
-var angkotBeroperasi = 9;
+var angkotBeroperasi = 5;
 var noAngkot = 1;
 
+for(noAngkot;noAngkot<=jmlAngkot;noAngkot++){
+  if(noAngkot<=angkotBeroperasi){
+    console.log(`Angkot no ${noAngkot} BAIK`)
+  }else{
+    console.log(`Angkot no ${noAngkot} RUSAK`)
+  }
+}
