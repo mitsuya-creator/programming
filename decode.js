2d28de61ca364b3429feb830b05678b708b2dc2a
mitsuya-creator <jj4068851@gmail.com>
2022-10-23 21:59:27 +0700

    decode1

--- decode.js
+++ decode.js
@@ -1,1 +1,14 @@
-const uri = 'https://nordvpn.com/what-is-my-ip'
+/*ENCODE is convert code to other languange
+  DECODE is reverse code to first
+*/
+
+
+const uri = 'https://mozilla.org/?x=шеллы'
+const encode = encodeURI(uri)
+console.log('ini Encode '+encode)
+
+try{
+  console.log('ini decode '+decodeURI(encode))
+}catch (e){
+  console.Error(e)
+}
