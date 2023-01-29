c5f8c59dfe1871420c4666f8c9ceb47021eb2df7
mitsuya-creator <jj4068851@gmail.com>
2023-01-30 03:14:30 +0700

    RzkyStore-v0.2

--- js/objCreate.js
+++ js/objCreate.js
@@ -4,9 +4,10 @@
 SELL = HARGA JUAL KE KONSUMEN
 BUY = HARGA BELI DARI TOKO/SALES
 */
-function NameCigarettes(merk,kind){
+function NameCigarettes(merk,kind,kas){
     this.merk = merk;
     this.kind = kind;
+    this.kas = kas;
     this.buy = function(kindOfCigarettes,amount) {
       for( i = 0; i <= this.kind.length; i++){
         if(this.kind.length == 0 || (this.kind.includes(kindOfCigarettes) == false)){
@@ -27,8 +28,29 @@ function NameCigarettes(merk,kind){
         }
     }
   }
+  
+  this.sell = function (kindOfCigarettes,amount,price){
+    for(i = 0; i <= this.kind.length;i++){
+      if(this.kind.length == 0 || (this.kind.includes(kindOfCigarettes) == false) ){
+        console.log("Out Of STOCK!");
+        return this.kind;
+      }
+      else if(this.kind[i] == kindOfCigarettes){
+        if(amount == undefined || this.kind[i+1] < amount){
+          console.log("input Is INCORRECT!")
+          return this.kind;
+        }
+        else{
+          this.kind.splice(i+1,1,kind[i+1]-amount);
+          this.kas = this.kas + (amount * price);
+          console.log(this.kind, this.kas)
+        }
+      }
+    }
+  }
 }
-let SM = new NameCigarettes("Sampoerna",[])
+let SM = new NameCigarettes("Sampoerna",[],0)
+
 
 
 
