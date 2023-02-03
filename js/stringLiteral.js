518ffe46da847590421f035467eb2c9b42396122
mitsuya-creator <jj4068851@gmail.com>
2023-02-04 04:42:57 +0700

    stringLiteral-v2-released

--- js/stringLiteral.js
+++ js/stringLiteral.js
@@ -7,23 +7,47 @@ let mhs = {
     "web programming",
     "data science",
     "data analyst"
-    ],
+      ],
     [
     "cooking vegetables",
     "beverage"
-    ]
+      ],
+    [
+      "business",
+      "marketing",
+      "akuntansi",
+      "tatakelola",
+      "archived",
+      "management"
+      ],
+    [
+      "aljabar",
+      "linear",
+      "aritmatika"
+      ]
     ]
 }
 
 // console.log(`hello everyone, my name is ${mhs.name} my age ${mhs.age} year.
 // ${(2+3)}
 // ${(mhs.age > 40 ? "old" : "young")} `)
+function printf(e){
+  let content= "";
+  for(i = 0; i < e.length; i++){
+    for(j = 0; j < e[i].length; j++){
+     content += `<li>${e[i][j]}</li>`;
+    }
+  }
+  return content;
+}
 
-const el = `<h1>DATA</h1>
-<span>name : ${mhs.name} </span><br>
-<span>age : ${mhs.age} </span>
-<ol> ${(mhs.study.map((std) => `<li>${std}</li>`).join(""))}</ol>
-`;
+
+
+
+const el = `<h1>DATA MHS</h1>
+<span>name : ${mhs.name}</span><br>
+<span>age : ${mhs.age}</span>
+<ol>${printf(mhs.study)}</ol>`;;
 document.body.innerHTML = el;
 
-console.log(mhs.study[0][0]); //pr how to print in dom seem in object ❌
+console.log(printf(mhs.study));
