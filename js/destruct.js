f292723649d817f84b39495356d1e51a70446057
mitsuya-creator <jj4068851@gmail.com>
2023-02-09 03:34:12 +0700

    destructringFunction

--- js/destruct.js
+++ js/destruct.js
@@ -1,21 +1,39 @@
-// Destructring Assignment
+// // Destructring Assignment
 
-// array
-//const mhs = ["halo","mitsuya","21"];
-//let [greeting,name,age] = mhs;
+// // array
+// //const mhs = ["halo","mitsuya","21"];
+// //let [greeting,name,age] = mhs;
 
-//console.log(`${greeting}, my name is ${name} , my age is ${age} years`)
+// //console.log(`${greeting}, my name is ${name} , my age is ${age} years`)
 
-// object
-let mhs = {
-  name: 'mitsuya',
-  age: 31,
-  email: 'mitsuya@gmail.com'
+// // object
+// let mhs = {
+//   name: 'mitsuya',
+//   age: 31,
+//   email: 'mitsuya@gmail.com'
+// }
+
+// let {name,age: umur,email} = mhs;
+// // obkect with function 
+// function getAge({age}){
+//   return age;
+// }
+// getAge(mhs);
+
+// destructing Function
+let calc = (a,b) => {
+  return {
+    sum: a+b,
+    reduce: a-b,
+    multiple: a*b,
+    divid: {
+      name: 'object in object'
+    }
+  }
 }
 
-let {name,age: umur,email} = mhs;
-// obkect with function 
-function getAge({age}){
-  return age;
-}
-getAge(mhs);
+console.log(result=calc(1,2),result.sum);
+
+function print({divid: {name}}){
+  console.log(`this's ${name} `);
+}print(result)
