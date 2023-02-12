069879dec594fe169cf018dc7593e301ae0958ae
mitsuya-creator <jj4068851@gmail.com>
2023-02-13 04:48:02 +0700

    spreadOP-v1-released

--- js/spreadOP.js
+++ js/spreadOP.js
@@ -1,5 +1,4 @@
 //SPREAD OPERATOR { ... }
-
 // copy Array
 const mhs = ['mitsuya','draken','mickey']
 const mhs1 = [...mhs];
@@ -7,27 +6,33 @@ const mhs1 = [...mhs];
 // Join Array
 const mhs2 = ['hina','diana','sanju']
 const mhs3 = [...mhs2,...mhs1]
-// let mhs3 = []
-// function push(fromArr,toArr){
-//   for(content of fromArr){
-//     toArr.push(content);
-//   }
-// }
 
+// IN OBJECT 
 let listOBJ ={
   obj1: mhs,
   obj2: mhs1,
   obj3: mhs2,
   obj4: mhs3
 }
-
 //indx tertentu 
-//PR looking for Mitsuya only in all property with position of mitsuya, Exam: in obj4 there's mitsuya in index 0  then print to console, PR❌❌❌
-for(m in listOBJ){
-  let newStr = []
-  let idx = parseInt(`${listOBJ[m].length - 3}`)
-  for(str of `${listOBJ[m][idx]}`){
-    newStr.push(`<span>${str}</span>`)
-  }
-  console.log(newStr.join(""))
+//PR looking for Mitsuya only in all property with position of mitsuya, Exam: in obj4 there's mitsuya in index 0  then print to console, ✅✅
+for(property in listOBJ){
+      let result,idx;
+      result = []
+      idx = `${listOBJ[property].length}`
+      console.log(`${property} = ${listOBJ[property]}`)
+      for(i = 0; i < idx; i++){
+        let content= listOBJ[property][i];
+        result.push(content);
+      }
+    if(result.includes("mitsuya") == true){
+      result.map((content,i) => {
+        if(content == "mitsuya"){
+        console.log(`IN ${property} idx[${i}] = ${content}`)
+        }
+      })
+    }
+    else{
+      console.log(`IN ${property} no mitsuya found `)
+    }
 }
