20bac7d2c42eeeae9383ff17d9b810c6d8146e4d
mitsuya-creator <jj4068851@gmail.com>
2023-02-20 05:13:37 +0700

    createElementIn-tbody

--- Rzkystore/rzkystore.js
+++ Rzkystore/rzkystore.js
@@ -15,7 +15,7 @@ function List_items(category,name,amount){
 //LIST_OBJECT ROKOK
 let laBold = new List_items("ROKOK","LA-BOLD",2);
 let mldBlack16 = new List_items("ROKOK","MLD-BLACK16",2);
-let djarcok = new List_items("ROKOK","DJARCOK",4);
+let djarcok = new List_items("ROKOK","DJARUM-COKLAT-KRETEK",4);
 let super12 = new List_items("ROKOK","SUPER12",4);
 let super50 = new List_items("ROKOK","SUPER50",6);
 let signatureBlk = new List_items("ROKOK","SIGNATURE-BLACK",5);
@@ -49,7 +49,38 @@ let mustika = new List_items("ROKOK","MUSTIKA",6);
 let magnumMax = new List_items("ROKOK","MAGNUM-MAX20",6);
 let onJasmin = new List_items("ROKOK","ON-JASMINE",3);
 let bintangMas = new List_items("ROKOK","BINTANG-MAS",6);
-let djavaHijau = new List_items("ROKOK","DJAVA-KRETEK-IJO",7);
+let djavaHijau = new List_items("ROKOK","DJAVA-IJO-KRETEK",7);
 let bhumi = new List_items("ROKOK","BHUMI",6);
 let esBerypop12 = new List_items("ROKOK","ESSE-BERRYPOP-12",6);
 let cakra = new List_items("ROKOK","CAKRA",6);
+
+// List Items ROKOK
+let listRokok = [laBold,mldBlack16,djarcok,super12,super50,signatureBlk,ggMerah,proMerah,proPutih,surya12,surya16,ggFilter12,ggFilter50,samsuKretek,magnumBlk12,samsuPrem,mbFb12,mbFb20,mbKretekBiru,mbIceburst,mbMerah,mbPutih,sm12,sm16,smKrtk,smMenthol16,esBerypop12,esBerypop16,esApplemint,esJuicy,camel,jazyBold,juaraKrtk,mustika,magnumMax,onJasmin,bintangMas,djavaHijau,bhumi,cakra];
+// 
+for(arrRokok of listRokok){
+  //console.log(arrRokok.name, arrRokok.amount)
+}
+
+// CREATE TABEL
+let table = `
+<table class="table">
+  <thead class="category table-dark">
+    <tr>
+      <td colspan="4" class="text-center fw-bold">ROKOK</td>
+    </tr>
+  </thead>
+  <thead class="table-secondary">
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">NAMA BARANG</th>
+      <th scope="col">JUMLAH BARANG(PCSs)</th>
+      <th scope="col">KETERANGAN</th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
+</table>
+`;
+document.getElementById("container").appendChild(document.createElement("div")).innerHTML= table;
+let tbody = document.querySelector("tbody");
+console.log(tbody)
