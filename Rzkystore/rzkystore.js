769ca98f2ac4dd92a5a31f465e6adb4c7cf1e13d
mitsuya-creator <jj4068851@gmail.com>
2023-02-22 03:31:39 +0700

    setFillRed-released

--- Rzkystore/rzkystore.js
+++ Rzkystore/rzkystore.js
@@ -19,7 +19,7 @@ let djarcok = new List_items("ROKOK","DJARUM-COKLAT-KRETEK",4);
 let super12 = new List_items("ROKOK","SUPER12",4);
 let super50 = new List_items("ROKOK","SUPER50",6);
 let signatureBlk = new List_items("ROKOK","SIGNATURE-BLACK",5);
-let ggMerah = new List_items("ROKOK","GG-MERAH",7);
+let ggMerah = new List_items("ROKOK","GG-MERAH-KRETEK",7);
 let proMerah = new List_items("ROKOK","PRO-MERAH",7);
 let proPutih = new List_items("ROKOK","PRO-PUTIH",2);
 let surya12 = new List_items("ROKOK","SURYA12",4);
@@ -31,7 +31,7 @@ let magnumBlk12 = new List_items("ROKOK","MAGNUM-BLACK12",7);
 let samsuPrem = new List_items("ROKOK","SAMSU-PREM",6);
 let mbFb20 = new List_items("ROKOK","MB-FB20",4);
 let mbFb12 = new List_items("ROKOK","MB-FB12",6);
-let mbKretekBiru = new List_items("ROKOK","MB-KRETEK-BIRU",6);
+let mbKretekBiru = new List_items("ROKOK","MB-BIRU-KRETEK",6);
 let mbIceburst = new List_items("ROKOK","MB-ICE-BURST",2);
 let mbMerah = new List_items("ROKOK","MB-MERAH",3);
 let mbPutih = new List_items("ROKOK","MB-PUTIH",4);
@@ -45,14 +45,14 @@ let esJuicy = new List_items("ROKOK","ESSE-JUICY",6);
 let camel = new List_items("ROKOK","CAMEL",6);
 let jazyBold = new List_items("ROKOK","JAZY-BOLD",5);
 let juaraKrtk = new List_items("ROKOK","JUARA-KRETEK",6);
-let mustika = new List_items("ROKOK","MUSTIKA",6);
+let mustika = new List_items("ROKOK","MUSTIKA-KRETEK",6);
 let magnumMax = new List_items("ROKOK","MAGNUM-MAX20",6);
-let onJasmin = new List_items("ROKOK","ON-JASMINE",3);
-let bintangMas = new List_items("ROKOK","BINTANG-MAS",6);
+let onJasmin = new List_items("ROKOK","ON-JASMINE-KRETEK",3);
+let bintangMas = new List_items("ROKOK","BINTANG-MAS-KRETEK",6);
 let djavaHijau = new List_items("ROKOK","DJAVA-IJO-KRETEK",7);
-let bhumi = new List_items("ROKOK","BHUMI",1);
+let bhumi = new List_items("ROKOK","BHUMI-KRETEK",1);
 let esBerypop12 = new List_items("ROKOK","ESSE-BERRYPOP-12",1);
-let cakra = new List_items("ROKOK","CAKRA",1);
+let cakra = new List_items("ROKOK","CAKRA-KRETEK",1);
 
 // List Items ROKOK
 let listRokok = [laBold,mldBlack16,djarcok,super12,super50,signatureBlk,ggMerah,proMerah,proPutih,surya12,surya16,ggFilter12,ggFilter50,samsuKretek,magnumBlk12,samsuPrem,mbFb12,mbFb20,mbKretekBiru,mbIceburst,mbMerah,mbPutih,sm12,sm16,smKrtk,smMenthol16,esBerypop12,esBerypop16,esApplemint,esJuicy,camel,jazyBold,juaraKrtk,mustika,magnumMax,onJasmin,bintangMas,djavaHijau,bhumi,cakra];
@@ -69,7 +69,7 @@ let table = `
     <tr>
       <th scope="col">NO</th>
       <th scope="col">NAMA BARANG</th>
-      <th scope="col">JUMLAH BARANG(PCSs)</th>
+      <th scope="col"></th>
       <th scope="col">KETERANGAN</th>
     </tr>
   </thead>
@@ -83,23 +83,25 @@ document.getElementById("container").appendChild(document.createElement("div")).
 let tbody = document.querySelector("tbody");
 // FOR NUMBER IN TBODY
 let noColumn = 1;
+// TR DNAGER TABLE
+//let trTableDanger = document.createElement("tr");
+//trTableDanger.setAttribute("class","table-danger");
+// TR TABLE NORMAL
+let trTableNormal = document.createElement("tr")
 // LOOPING FOR LIST ITEM
 for(arrContent of listRokok){
-  let trTable = `
+  let trTableContent = `
       <th scope="row">${noColumn++}</th>
       <td>${arrContent.name}</td>
-      <td class="text-center">${arrContent.amount}</td>
+      <td class="text-center">...</td>
       <td>${arrContent.adverb()}</td>`;
-    if(arrContent.adverb() == "KURANG"){❌❌
-      let trTable = tbody.appendChild(document.createElement("tr")).setAttribute("class","table-danger");
-      trTable.
-      console.log(trTable);
-      let trDanger = document.getElementsByClassName("table-danger");
-      for(i = 0; i < trDanger.length; i++){
-        trDanger[i].innerHTML = trTable;
-      }
-    }❌❌❌
-    else{
-      tbody.appendChild(document.createElement("tr")).innerHTML=trTable;✅
+  if(arrContent.adverb() == "KURANG"){
+    let trTableDanger = tbody.appendChild(document.createElement("tr"));
+    trTableDanger.setAttribute("class","table-danger");
+    trTableDanger.innerHTML = trTableContent;
+    //tbody.appendChild().innerHTML = trTableContent;
     }
+  else{
+    tbody.appendChild(document.createElement("tr")).innerHTML=trTableContent;
+  }
 }
