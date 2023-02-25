a027a902fa11a1fb22712e43414a2e117bd8a189
mitsuya-creator <jj4068851@gmail.com>
2023-02-26 06:01:00 +0700

    Map()

---
+++ dbsDicoding/map.js
@@ -0,0 +1,16 @@
+// Map()
+let mitsuya = new Map(
+  [
+     ["name", "mitsuya" ],
+     [true, false],
+     ["name of school", "SMK SUKALUYU"]
+  ]
+);
+  
+console.log(mitsuya.get("name"));
+console.log(mitsuya.size)
+mitsuya.set("gf", 404); // add gf
+console.log(mitsuya.get("gf"));
+console.log(mitsuya.size)
+
+
