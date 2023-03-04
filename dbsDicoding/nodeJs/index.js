b57b923a772518df01800d8dae6c8b47c456b70f
mitsuya-creator <jj4068851@gmail.com>
2023-03-04 08:21:07 +0700

    report4_3_23

--- dbsDicoding/nodeJs/index.js
+++ dbsDicoding/nodeJs/index.js
@@ -1,1 +1,25 @@
-console.log(test)
+// > LAKUKAN PENGECEKAN KETERSEDIAN MESIN
+// > PERIKSA STOCK DIDALAM MESIN
+// > BUAT KOPI
+
+const state = {
+  stock: {
+    coffee : 500,
+    water : 1000
+  },
+  isCoffeeMachineBusy: false
+}
+console.log('console-1')
+const checkAvaibility = () => {
+  return new Promise((resolve,reject) =>{
+    setTimeout(() => {
+    if(state.stock.coffee > 100 && state.stock.water > 50){
+      resolve('Material ready to used')
+    }
+    else{
+      reject('bahan kurang')
+    }
+  }, 5000)})
+}
+console.log(setTimeout(() = > checkAvaibility(),1000))
+console.log('console-2')
