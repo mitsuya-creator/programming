736282e044d38d166cd21c8ced0c57f8d0aaa61b
mitsuya-creator <jj4068851@gmail.com>
2023-03-07 04:22:01 +0700

    asyncAwait

--- dbsDicoding/nodeJs/index.js
+++ dbsDicoding/nodeJs/index.js
@@ -1,16 +1,28 @@
-// ALGORITHM MAKING COFFEE
-// >CHECK MACHINE COFFEE
-// >CHECK STOCK MATERIAL COFFEE
-// >BREWING COFFEE
-// >COFFE READY
-const water = require('./components/boilingWater');
-class state{
-  constructor(name,water){
-    this.name = name;
-    this.water = water;
+// ASYNC AWAIT
+
+//Promise GetCofee() 
+const getCoffee = () => {
+  return new Promise((resolve,reject) => {
+    let seeds = 1000;
+    if(seeds >= 100){
+      resolve('Coffee Ready')
+    }else{
+      reject('We need seeds coffee more')
+    }
+  })
+}
+//Using then()
+// function makingCoffe(){
+//   getCoffee().then(resolve => console.log(resolve)).catch(reject => console.log(reject));
+// }
+
+//Using Async / await
+async function makingCoffe() {
+  try{
+  const coffee = await getCoffee();
+  console.log(coffee);
+  }catch(reject){
+    console.log(reject);
   }
 }
-
-const expressoCoffee = new state('Expresso',100);
-console.log(expressoCoffee)
-console.log(boilingWater()) 
+makingCoffe();
