a873ce714f94774cd1d3a754e505b27b6fec449f
mitsuya-creator <jj4068851@gmail.com>
2023-03-08 05:14:27 +0700

    npmLodash

--- dbsDicoding/nodeJs/index.js
+++ dbsDicoding/nodeJs/index.js
@@ -1,76 +1,16 @@
-/**
- * Ini adalah program untuk mendapatkan nama user dari internet.
- * Terdapat dua fungsi yang sudah dibuat, berikut penjelasanya:
- *   - fetchingUserFromInternet:
- *     - fungsi ini digunakan untuk mendapatkan data user seolah-olah dari internet.
- *     - fungsi ini menerima dua argumen yakni callback, dan isOffline.
- *     - Argumen callback membawa dua nilai yakni error dan user:
- *       - error: NetworkError akan dibawa oleh callback bila isOffline bernilai true.
- *       - user: data user akan dibawa oleh callback bila isOffline bernilai false.
- *   - gettingUserName:
- *      - fungsi ini memanggil fungsi fetchingUserFromInternet dengan nilai isOffline: false untuk mendapatkan data user name dari internet.
- *      - fungsi ini harus mengembalikan nilai user.name, namun sulit karena menggunakan pola callback.
- *      - Maka dari itu, ubahlah fetchingUserFromInternet dari callback menjadi promise
- *      - Dengan begitu, Anda bisa memanfaatkan .then atau async/await untuk mendapatkan user.name.
- *
- * TODO: 1
- * - Ubahlah fungsi fetchingUserFromInternet dengan memanfaatkan Promise. Anda bisa menghapus implementasi callback.
- *
- * TODO: 2
- * - Ubahlah cara mengonsumsi fungsi fetchingUserFromInternet dari callback ke Promise.
- * - Tips:
- *   - Agar penulisan kode lebih bersih dan mudah dibaca, coba manfaatkan async/await
- *
- *
- * Notes:
- * - Jangan ubah struktur atau nilai dari objek user yang dibawa oleh callback sebelumnya.
- * - Tetap gunakan NetworkError untuk membawa nilai error pada Promise
- */
- class NetworkError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'NetworkError';
-  }
-}
+// LODASH
 
-// TODO: 1
-// const fetchingUserFromInternet = (callback, isOffline) => {
-//   setTimeout(() => {
-//     if (isOffline) {
-//       callback(new NetworkError('Gagal mendapatkan data dari internet'), null);
-//     }
-//     callback(null, { name: 'John', age: 18 });
-//   }, 500);
-// };
+//Without Lodash
+// const myArr = [1,2,3,4];
+// let sum = 0;
+// for(i = 0; i < myArr.length; i++){
+//   sum+=myArr[i];
+// }
 
-const fetchingUserFromInternet = (isOffline) => {
-  return new Promise((resolve,reject) =>{
-    setTimeout(()=>{
-      if(isOffline){
-        reject(new NetworkError('Gagal mendapatkan data dari internet'));
-      }else{
-        resolve({name: 'John', age: 18})
-      }
-    },1000)
-  })
-}
-// TODO: 2
-// const gettingUserName = () => {
-//   fetchingUserFromInternet((error, user) => {
-//     if (error) {
-//       return error.message;
-//     }
-//     return user.name;
-//   }, false);
-// };
 
-async function gettingUserName(){
-  try{
-  const getUser = await fetchingUserFromInternet(false)
-  return getUser.name;
-  }catch(reject){
-    console.log(reject);
-  }
-}
+//With lodash
+import _ from "lodash";
+const myArr = [1,2,3,4,5]
+const sum = _.sum(myArr);
 
-gettingUserName()
+console.log(sum);
