2925a3bc1d2d96a4d1b48f44828de2050523bbd2
mitsuya-creator <jj4068851@gmail.com>
2023-03-09 04:58:06 +0700

    testJest

--- dbsDicoding/nodeJs/Testing/_test_/gradeCalculation.test.js
+++ dbsDicoding/nodeJs/Testing/_test_/gradeCalculation.test.js
@@ -1,4 +1,4 @@
-import {averageExams, isStudentPassExam} from '../gradeCalculation.js';
+const {averageExams, isStudentPassExam} = require('../gradeCalculation.js');
 test('it should return exact average', () => {
   const listValueExams = [100,80,80,100];
   expect(averageExams(listValueExams)).toEqual(90);
