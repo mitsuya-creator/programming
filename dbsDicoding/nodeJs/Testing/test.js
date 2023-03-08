5da2d23f222373b1034045bd40fd900824366274
mitsuya-creator <jj4068851@gmail.com>
2023-03-09 03:43:51 +0700

    Testing

---
+++ dbsDicoding/nodeJs/Testing/test.js
@@ -0,0 +1,5 @@
+import {averageExams, isStudentPassExam} from './gradeCalculation.js';
+
+const scoreBudi = [100,0,0,200];
+
+console.log(isStudentPassExam(scoreBudi,'budi'));
--- dbsDicoding/nodeJs/index.js
+++ dbsDicoding/nodeJs/index.js
@@ -1,16 +1,3 @@
-// LODASH
-
-//Without Lodash
-// const myArr = [1,2,3,4];
-// let sum = 0;
-// for(i = 0; i < myArr.length; i++){
-//   sum+=myArr[i];
-// }
-
-
-//With lodash
-import _ from "lodash";
-const myArr = [1,2,3,4,5]
-const sum = _.sum(myArr);
-
-console.log(sum);
+import {averageExams, isStudentPassExam} from './Testing/gradeCalculation.js';
+const score = [1,2,3];
+console.log(averageExams(score));
