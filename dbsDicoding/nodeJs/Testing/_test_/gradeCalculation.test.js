17c88b14949eb2f1c7f8dbeba804b10a042b8e6c
mitsuya-creator <jj4068851@gmail.com>
2023-03-09 07:58:19 +0700

    integratedAnd_Unitest

--- dbsDicoding/nodeJs/Testing/_test_/gradeCalculation.test.js
+++ dbsDicoding/nodeJs/Testing/_test_/gradeCalculation.test.js
@@ -1,5 +1,23 @@
+//GROUPING UNITEST AND INTEGRATED TEST
 const {averageExams, isStudentPassExam} = require('../gradeCalculation.js');
-test('it should return exact average', () => {
-  const listValueExams = [100,80,80,100];
-  expect(averageExams(listValueExams)).toEqual(90);
-});
+
+describe('UNITEST AND INTEGRATED TEST gradeCalculation', () => {
+  test('it should return exact average', () => {
+    const listValueExams = [100,80,80,100];
+    expect(averageExams(listValueExams)).toEqual(90);
+  });
+  
+  /**
+   * INTEGRATED TEST
+   * */
+  test('it should return pass Exam True',() =>{
+    const scoreMitsuya = [100,80,80,95];
+    expect(isStudentPassExam(scoreMitsuya,"Mitsuya")).toEqual(true);
+  });
+  
+  test('it should return pass Exam False',() =>{
+    const scoreBudi = [8,7,9,78];
+    expect(isStudentPassExam(scoreBudi,"Budi")).toEqual(false);
+  });
+  
+})
