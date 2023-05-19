89d57fbc6c6624c86535dfdd81f15ef1721ec797
projectx <@test.com>
2023-05-19 23:37:58 +0700

    module

---
+++ index.js
@@ -0,0 +1,5 @@
+import {sum,div, multiple, subtract} from './aritmatic.js';
+console.log(`sum = ${sum(3,5)}`);
+console.log(`div = ${div(3,5)}`);
+console.log(`multiple = ${multiple(3,5)}`);
+console.log(`subtract = ${subtract(3,5)}`);
---
+++ messages.js
@@ -0,0 +1,6 @@
+const messages = () =>{
+  const name =  'mitsuya';
+  const age = 22;
+  return console.log(`name ${name}, age ${age}`);
+}
+export default messages;
